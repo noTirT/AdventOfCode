697578eb49a72d772d550eb761c9475f11ad9d6a
Tom Manger <tommanger55@gmail.com>
2024-06-11 23:09:56 +0200

    updates

--- 2022/day3/part2.go
+++ 2022/day3/part2.go
@@ -7,9 +7,36 @@ import (
 )
 
 func Solution02() int {
-    lines := util.GetFileLines("./day3/input.txt")
+    lines := util.GetFileLines("./day3/test.txt")
 
     result := 0
+
+    for i := 0; i<len(lines); i+=3{
+        group := lines[i:i+3]
+
+        chars1 := strings.Split(group[0], "")
+        chars2 := strings.Split(group[1], "")
+        chars3 := strings.Split(group[2], "")
+
+        hash := make(map[string]struct{})
+        for j := 0; j < len(chars1); j++{
+            hash[chars1[j]] = struct{}{}
+        }
+
+        for j := 0; j<len(chars2); j++{
+            hash[chars2[j]] = struct{}{}
+        }
+
+        var common string
+        for j := 0; j<len(chars3); j++{
+            if _,ok := hash[chars3[j]]; ok{
+                common = chars3[j] 
+            }
+        }
+
+        println(common)
+
+    }
 
     return result 
 
---
+++ 2022/day4/part1.go
@@ -0,0 +1,18 @@
+package day4
+
+import (
+	"strings"
+
+	"github.com/noTirT/AdventOfCode/2022/util"
+)
+
+func Solution01() int {
+    lines := util.GetFileLines("./day4/test.txt")
+
+    result := 0
+    for _, line := range lines {
+        pair := strings.Split(line, ",")
+
+    }
+    return result
+}
